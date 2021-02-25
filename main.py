0bd0b75909812e36426f90b715d9a5e65acefbbf
Madhav Anand <madhav1004@icloud.com>
2021-02-25 22:21:35 +0530

    changed gitignore and added blockchain yet need to test.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-import flask
+# Importing libraries
+
+import datetime
+import hashlib
+import json
+import time
+from flask import Flask, jsonify
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+
+    def create_block(self, proof, prev_hash):
+        block = {
+
+        }
+
+
+
+block_chain = Blockchain()
